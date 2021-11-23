8813b4f55ca513f47f70467d2a769067102016f0
beer <bereri@norlys.dk>
2021-11-23 12:15:21 +0100

    jørgen now jumps
    .

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,27 +7,34 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         
-
+        if (Input.GetButtonDown("Jump"))
+        {jump = true;} 
+        
+        if (Input.GetButtonDown("Crouch"))
+        {crouch = true;}
     }
 
     // Moving the character function
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+        crouch = false;
     }
 
 }
