814f028d9c8b3199b56ff69caf54e575b95ac4f6
Lukas Koch Vindbjerg <201906015@post.au.dk>
2021-11-25 14:28:03 +0100

    Imported Viking Sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Health = FindObjectOfType<Stats>().Health;
+        // Health = FindObjectOfType<Stats>().Health;
         
     }
     
