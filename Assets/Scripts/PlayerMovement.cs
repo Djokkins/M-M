7c14bbc9d25e0c808792312698a987e6eae33700
jenner231 <jenner231@users.noreply.github.com>
2021-12-13 15:14:10 +0100

    Pixel size
    Set pixel per unit to 32 instead of 100 for all our animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
     float horizontalMove = 0f;
