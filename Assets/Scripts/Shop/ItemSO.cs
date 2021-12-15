050522498bfc36bd4a7b39d77788461536455020
Lukas Koch Vindbjerg <201906015@post.au.dk>
2021-12-15 16:13:44 +0100

    Work on shop and Scriptable Objects for our items

--- Assets/Scripts/Shop/ItemSO.cs
+++ Assets/Scripts/Shop/ItemSO.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ShopMenu", menuName = "Scriptable Objects/New Show Item", order = 1)]
-public class ShopItemSO : ScriptableObject 
+[CreateAssetMenu(fileName = "Menu", menuName = "Scriptable Objects/New Item", order = 1)]
+public class ItemSO : ScriptableObject 
 {
+    public string id;
+
     public string title;
     public string description;
     public int baseCost; 
 
     public Sprite itemImg;   
+
+    public GameObject prefab;
 }
 
